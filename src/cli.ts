983f7700b72bf0622adbf9cc0760e6cba064f417
so1ve <nn_201312@163.com>
2022-12-06 17:37:12 +0800

    feat: new project

--- src/cli.ts
+++ src/cli.ts
@@ -1,5 +1,24 @@
 import { Clerc } from "clerc";
+import { helpPlugin } from "@clerc/plugin-help";
+import { completionsPlugin } from "@clerc/plugin-completions";
+import { notFoundPlugin } from "@clerc/plugin-not-found";
 
-Clerc.create()
-  
-  .parse();
+import packageJSON from "../package.json";
+
+import newCommand from "./commands/new";
+
+async function main () {
+  const { name, description, version } = packageJSON;
+
+  Clerc.create()
+    .name(name)
+    .description(description)
+    .version(version)
+    .use(helpPlugin())
+    .use(completionsPlugin())
+    .use(notFoundPlugin())
+    .command(newCommand)
+    .parse();
+}
+
+main();
